737b129e47a10b55b15c4954734aeb4553b16e32
tferic <toni.feric@belsoftnet.local>
2019-01-08 01:46:49 +0100

    Problem001 Perl : 1st solution

--- perl/euler-001.pl
+++ perl/euler-001.pl
@@ -1,4 +1,22 @@
+=pod
+Problem 1: Multiples of 3 and 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+https://projecteuler.net/problem=1
+=cut
 
-#!/usr/bin/env perl
-# just a hello world
-print "hello world\n"
+use strict;
+
+sub sumMultiples {
+  my $maxval = shift @_ ;
+  my $sum = 0 ;
+  foreach my $i (1..($maxval-1)) {
+    if(( $i % 3 == 0) || ( $i % 5 == 0 )) {
+        $sum += $i ;
+    }
+  }
+  return $sum;
+}
+
+print "Sum(10): ",sumMultiples(10),"\n" ;
+print "Sum(1000): ",sumMultiples(1000),"\n" ;
