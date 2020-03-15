147ddb14c5eea50ac601120683631357e08badae
Toni Feric <toni@macmini-lan.feric.ch>
2020-03-16 00:55:52 +0100

    Change comment line

--- julia/euler-004.jl
+++ julia/euler-004.jl
@@ -1,7 +1,7 @@
 #=
 Problem 4: Largest palindrome pal_testuct
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-Find the largest palindrome made from the pal_testuct of two 3-digit numbers.
+Find the largest palindrome made from the product of two 3-digit numbers.
 https://projecteuler.net/problem=4
 =#
 
